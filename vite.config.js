13e9dde0f1955987cc3d80f0f8073d892d930122
cgchung1227 <cgchung1227@gmail.com>
2023-09-07 11:47:20 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'? '/React-gh-pages-sample.github.io/': '/',
+  base: process.env.NODE_ENV === 'production' ? '/cindychung-portfolio/' : '/',
   plugins: [react()],
 })
+
